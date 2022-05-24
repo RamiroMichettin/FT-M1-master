698d9ebb8de25f76380a443f10c8df15c19398bc
RamiroMichettin <ramiromichetti@gmail.com>
2022-05-23 22:31:09 -0300

    Homework1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,25 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+      sum += num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
+  }
 
-}
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  let array = [];
+  let i = num;
+  do {
+    let resto = i % 2;
+    array.push(Math.floor(resto));
+    i = i / 2;
+  } while (i >= 1);
+  return array.reverse().join("");
 }
+
 
 
 module.exports = {
